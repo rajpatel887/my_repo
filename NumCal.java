d91b8440786904e3c5c298230e7e70a5a09318ea
rajpatel887 <rp076772@gmail.com>
2024-12-22 00:32:28 +0530

    v1

---
+++ NumCal.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class NumCal {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int a, b, c;
+	System.out.println("Enter 1st number");
+	a = sc.nextInt();
+	System.out.println("Enter 2ed number");
+	b = sc.nextInt();
+	System.out.println("Enter 3ed number");
+	c = sc.nextInt();
+	
+	int sum = a+b+c;
+	System.out.println("sum of numbers are " + sum);
+//	System.out.println("sum of numbers are " + sum);
+//	System.out.println("avg of numbers are " + sum/3);
+//	System.out.println("avg of numbers are " + sum/3);
+	
+	
+	
+	sc.close();
+}
+}
+
